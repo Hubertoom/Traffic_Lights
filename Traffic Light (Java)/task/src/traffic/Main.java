0d86327fa5ed5ad61965c0baa6a60905e62ccb52
Hubertoom <h.michalec@o2.pl>
2023-06-27 14:00:43 +0200

    stage 1 done

--- Traffic Light (Java)/task/src/traffic/Main.java
+++ Traffic Light (Java)/task/src/traffic/Main.java
@@ -2,6 +2,15 @@ package traffic;
 
 public class Main {
   public static void main(String[] args){
-    System.out.println("Hello World!");
+    displayMenu();
+  }
+
+  private static void displayMenu() {
+    System.out.println("Welcome to the traffic management system!");
+    System.out.println("Menu:\n" +
+            "1. Add\n" +
+            "2. Delete\n" +
+            "3. System\n" +
+            "0. Quit");
   }
 }
